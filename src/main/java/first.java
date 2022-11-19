bee1e9ce5aae17ae3e668d30345f8d86fc24a6a4
unknown <ayazbi99@icloud.com>
2022-11-20 02:54:58 +0500

    second commit

---
+++ src/main/java/first.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.IOException;
+public class first {
+
+    public static void main (String[] args) throws IOException {
+        String  b = "Apple";
+
+        System.out.println("What kind of fruit do I like?");
+        System.out.print("Try to guess:");
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+
+        if (a.equalsIgnoreCase(b)){ //проверяет equalsIgnoreCase string \\
+            System.out.println("Excellent");
+        }
+        else{
+            System.out.println("Try to next time");
+        }
+    }
+
+}
